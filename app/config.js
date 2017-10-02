3f166bbcdfcdc62d34b82fa1b8eabd8fbd5d06b2
Salvador Montiel <salvadormontiel8@gmail.com>
2017-10-01 23:26:42 -0500

    Initial commit

---
+++ app/config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var config = {
+    apiKey: "AIzaSyDNjBoRK94KiJXQp8hj7LFFvXUUU7vPfbI",
+    authDomain: "paradox-b4cd2.firebaseapp.com",
+    databaseURL: "https://paradox-b4cd2.firebaseio.com",
+    projectId: "paradox-b4cd2",
+    storageBucket: "paradox-b4cd2.appspot.com",
+    messagingSenderId: "764520975075"
+};
+firebase.initializeApp(config);
